48dab616dcbe871b3bae4b68927183b340f49b7b
EarthTurtle <43360323+EarthTurtleDerp@users.noreply.github.com>
2020-08-13 14:39:36 -0400

    added creative tab

--- src/main/java/com/earthturtle/firstly/Firstly.java
+++ src/main/java/com/earthturtle/firstly/Firstly.java
@@ -2,6 +2,8 @@ package com.earthturtle.firstly;
 
 
 import com.earthturtle.firstly.util.RegistryHandler;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -39,6 +41,15 @@ public class Firstly
     private void doClientStuff(final FMLClientSetupEvent event) {
 
     }
+
+    public static final ItemGroup TAB = new ItemGroup("Firstly") {
+
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(RegistryHandler.RUBY.get());
+        }
+
+    };
 
 
 }
--- src/main/java/com/earthturtle/firstly/items/ItemBase.java
+++ src/main/java/com/earthturtle/firstly/items/ItemBase.java
@@ -1,11 +1,12 @@
 package com.earthturtle.firstly.items;
 
+import com.earthturtle.firstly.Firstly;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
 public class ItemBase extends Item {
 
     public ItemBase() {
-        super(new Item.Properties().group(ItemGroup.MATERIALS));
+        super(new Item.Properties().group(Firstly.TAB));
     }
 }
